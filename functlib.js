26fa6819e1013b3fdc5cdf10440a91d99a1c3a8b
Michael L Parks <github@wmi-inc.com>
2017-07-06 14:57:28 -0400

    Refactored and Added Victory Sound

--- functlib.js
+++ functlib.js
@@ -7,9 +7,13 @@ function current_turn(xTurn){
   }
 }
 
+function get_square(squareNumber){
+  return allSquares[squareNumber - 1];
+}
+
 function check_row_one(){
-  if(s1.innerHTML !== ''){
-    if((s1.innerHTML === s2.innerHTML) && (s1.innerHTML === s3.innerHTML)){
+  if(get_square(1).innerHTML !== ''){
+    if((get_square(1).innerHTML === get_square(2).innerHTML) && (get_square(1).innerHTML === get_square(3).innerHTML)){
       return true;
     }else{
       return false;
@@ -19,8 +23,8 @@ function check_row_one(){
   }
 }
 function check_row_two(){
-  if(s4.innerHTML !== ''){
-    if((s4.innerHTML === s5.innerHTML) && (s4.innerHTML === s6.innerHTML)){
+  if(get_square(4).innerHTML !== ''){
+    if((get_square(4).innerHTML === get_square(5).innerHTML) && (get_square(4).innerHTML === get_square(6).innerHTML)){
       return true;
     }else{
       return false;
@@ -30,8 +34,8 @@ function check_row_two(){
   }
 }
 function check_row_three(){
-  if(s7.innerHTML !== ''){
-    if((s7.innerHTML === s8.innerHTML) && (s7.innerHTML === s9.innerHTML)){
+  if(get_square(7).innerHTML !== ''){
+    if((get_square(7).innerHTML === get_square(8).innerHTML) && (get_square(7).innerHTML === get_square(9).innerHTML)){
       return true;
     }else{
       return false;
@@ -50,8 +54,8 @@ function check_rows(){
 }
 
 function check_column_one(){
-  if(s1.innerHTML !== ''){
-    if((s1.innerHTML === s4.innerHTML) && (s1.innerHTML === s7.innerHTML)){
+  if(get_square(1).innerHTML !== ''){
+    if((get_square(1).innerHTML === get_square(4).innerHTML) && (get_square(1).innerHTML === get_square(7).innerHTML)){
       return true;
     }else{
       return false;
@@ -62,8 +66,8 @@ function check_column_one(){
 }
 
 function check_column_two(){
-  if(s2.innerHTML !== ''){
-    if((s2.innerHTML === s5.innerHTML) && (s2.innerHTML === s8.innerHTML)){
+  if(get_square(2).innerHTML !== ''){
+    if((get_square(2).innerHTML === get_square(5).innerHTML) && (get_square(2).innerHTML === get_square(8).innerHTML)){
       return true;
     }else{
       return false;
@@ -74,8 +78,8 @@ function check_column_two(){
 }
 
 function check_column_three(){
-  if(s3.innerHTML !== ''){
-    if((s3.innerHTML === s6.innerHTML) && (s3.innerHTML === s9.innerHTML)){
+  if(get_square(3).innerHTML !== ''){
+    if((get_square(3).innerHTML === get_square(6).innerHTML) && (get_square(3).innerHTML === get_square(9).innerHTML)){
       return true;
     }else{
       return false;
@@ -94,8 +98,8 @@ function check_columns(){
 }
 
 function check_diagonal_one(){
-  if(s1.innerHTML !== ''){
-    if((s1.innerHTML === s5.innerHTML) && (s1.innerHTML === s9.innerHTML)){
+  if(get_square(1).innerHTML !== ''){
+    if((get_square(1).innerHTML === get_square(5).innerHTML) && (get_square(1).innerHTML === get_square(9).innerHTML)){
       return true;
     }else{
       return false;
@@ -105,8 +109,8 @@ function check_diagonal_one(){
   }
 }
 function check_diagonal_two(){
-  if(s3.innerHTML !== ''){
-    if((s3.innerHTML === s5.innerHTML) && (s3.innerHTML === s7.innerHTML)){
+  if(get_square(3).innerHTML !== ''){
+    if((get_square(3).innerHTML === get_square(5).innerHTML) && (get_square(3).innerHTML === get_square(7).innerHTML)){
       return true;
     }else{
       return false;
@@ -148,11 +152,29 @@ function diagonal_check(){
   }
 }
 
+function you_win(winner){
+  audio.play();
+  console.log('Start Alert...');
+  setTimeout(function(){
+    alert('The WINNER is '+winner+'!!!');
+  }, 1200);
+}
+
+function winning_alert(winner){
+  setTimeout(function(){
+    alert('The WINNER is '+winner+'!!!');
+  }, 1600);
+}
+
+// var audio = new Audio('audio_file.mp3');
+// audio.play();
+
 function check_for_win(){
   if(horizontal_check() || vertical_check() || diagonal_check()){
     var winner = current_turn(xTurn);
-    setTimeout(function(){ alert('The WINNER is '+winner+'!!!');}, 100);
-    setTimeout(function(){ clear_board();}, 100);
+    // setTimeout(function(){ alert('The WINNER is '+winner+'!!!');}, 50);
+    you_win(winner);
+    setTimeout(function(){ clear_board();}, 1600);
   }else{
     return false;
   }
--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-var s1 = document.getElementById('1');
-var s2 = document.getElementById('2');
-var s3 = document.getElementById('3');
-var s4 = document.getElementById('4');
-var s5 = document.getElementById('5');
-var s6 = document.getElementById('6');
-var s7 = document.getElementById('7');
-var s8 = document.getElementById('8');
-var s9 = document.getElementById('9');
+var allSquares = document.getElementsByClassName("btn");
+var audio = new Audio('./sound/trumpet.wav');
 var xTurn = true;
 var xColor = 'red';
 var yColor = 'blue';
-var allSquares = document.getElementsByClassName("btn");
+
 for(i = 0; i < allSquares.length; i++){
   allSquares[i].addEventListener('click', function(e){
-      // console.log('this button was clicked...');
-      // console.log(e.target);
       displayValue = e.target.innerHTML;
-      // console.log(displayValue);
-
       if(displayValue !== 'X' && displayValue !== 'O'){
         if(xTurn){
           e.target.style.backgroundColor = xColor;
